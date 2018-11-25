2a4f1c55e9218b5f2d1fd11eeb211ef5a64d2fec
ho <hoquangnam45@gmail.com>
2018-11-25 20:23:11 +0700

    First commit

---
+++ app/src/main/java/com/iot/hoquangnam/lockerinteractiveapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.iot.hoquangnam.lockerinteractiveapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Trang chủ");
+    }
+}
